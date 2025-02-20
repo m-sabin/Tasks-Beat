d798e540bca7c5c5e44e1d578e35827a8a0d79e2
nibass <contato.ecomcenter@gmail.com>
2025-02-20 19:20:08 -0300

    Inserir as categorias

--- app/src/main/java/com/devspace/taskbeats/CategoryDao.kt
+++ app/src/main/java/com/devspace/taskbeats/CategoryDao.kt
@@ -2,6 +2,7 @@ package com.devspace.taskbeats
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
@@ -10,6 +11,6 @@ interface CategoryDao {
     @Query("Select * From categoryentity")
     fun getAll(): List<CategoryEntity>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(categoryEntity: List<CategoryEntity>)
 }
